bb3d5bd40eec1d27c38e7887820508f0aac4f3b5
admin <admin@192.168.43.224>
2017-11-10 12:39:44 +0700

    ควยบอส

---
+++ LabGit/src/controller/Songsak.java
@@ -0,0 +1,26 @@
+package controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
+class Songsak {
+	
+	@Before
+	private void setup() throws Exception{
+		// TODO Auto-generated method stub
+
+	}
+	@After
+	private void teardown() throws Exception{
+		// TODO Auto-generated method stub
+
+	}
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
